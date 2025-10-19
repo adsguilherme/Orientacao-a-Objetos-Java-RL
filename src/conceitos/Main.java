9e466ef9309163cffc95cf174eb2e91579d09537
gscode <adm.guilhermedesousa@gmail.com>
2025-10-19 13:10:26 -0300

    study: java - orientação a objetos

---
+++ src/conceitos/Main.java
@@ -0,0 +1,24 @@
+package conceitos;
+
+public class Main {
+
+    void main() {
+
+        // Criar ou Instanciar objeto da classe Pessoa
+        Pessoa p = new Pessoa();
+
+        // O objeto p tem acesso aos atributos e métodos da classe Pessoa
+        p.nome = "Guilherme";
+        p.idade = 39;
+
+        p.apresentacao();
+
+        //String situacao = p.situacaoIdade();
+        //System.out.println("A situação da idade é: " + situacao);
+
+        // Fiz a chamada direta do metodo sem criar uma variável
+        System.out.println("A situação da idade é: " + p.situacaoIdade());
+
+
+    }
+}
---
+++ src/conceitos/Pessoa.java
@@ -0,0 +1,19 @@
+package conceitos;
+
+public class Pessoa {
+
+    // Atributos
+    String nome;
+    int idade;
+
+    // Metodo de apresentação
+    // Que não a retorno
+    void apresentacao() {
+        System.out.println("Olá " + nome + " você tem " + idade + " anos.");
+    }
+
+    // Metodo para retornar a situacao
+    String situacaoIdade() {
+        return idade >= 18 ? "Maior de idade" : "Menor de idade";
+    }
+}
